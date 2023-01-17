fbf3d1cf647644edfbf8315ef579bbbe7b451918
CRAiespaco <christopherrodriguea.alu@iespacomolla.es>
2023-01-17 16:52:34 +0100

    17/01/2023

---
+++ resources/views/pistas/index.blade.php
@@ -0,0 +1,6 @@
+@extends('predefinido')
+
+@section('contenido')
+
+
+@endsection
---
+++ resources/views/pistas/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('contenido')
+    <section class="contenidoPrincipal">
+        <div>
+            <p>mensaje de prueba</p>
+        </div>
+    </section>
