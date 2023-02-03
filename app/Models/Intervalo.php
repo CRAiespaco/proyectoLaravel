c0f11eaa9fccef47a1f7b7a6abae41f673ddf4ef
CRAiespaco <christopherrodriguea.alu@iespacomolla.es>
2023-02-03 18:26:13 +0100

    3/02/2023 cosas del proyecto final

--- app/Models/Intervalo.php
+++ app/Models/Intervalo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Intervalo extends Model
 {
     use HasFactory;
+
+    public function horario(){
+        return $this->belongsTo(Horario::class);
+    }
+
+    public function receta(){
+        return $this->belongsTo(Receta::class);
+    }
 }
